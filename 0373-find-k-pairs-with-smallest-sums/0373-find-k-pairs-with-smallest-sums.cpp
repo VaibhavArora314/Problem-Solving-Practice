062d2df302315d954676d514231a918e41035250
Vaibhav Arora <84830429+VaibhavArora314@users.noreply.github.com>
2023-06-27 18:53:33 +0530

    Time: 291 ms (81.85%), Space: 114.3 MB (47.63%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -3,18 +3,19 @@ public:
  vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<vector<int>> ans;
         priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> > pq;
-        map<vector<int>,int> m;
+        set<pair<int,int>> visited;
 
         pq.push({nums1[0]+nums2[0],{0,0}});
+        // visited.insert({0,0});
 
         for (int i=0;i<k && !pq.empty();i++) {
             pair<int,pair<int,int>> t = pq.top();
-            int b =t.second.first,a=t.second.second;
+            int a =t.second.first,b=t.second.second;
 
             ans.push_back({nums1[a],nums2[b]});
 
-            if (b+1 < nums2.size() && m.find({b+1,a}) == m.end()) {pq.push({nums1[a]+nums2[b+1],{b+1,a}});m[{b+1,a}] = 1;}
-            if (a+1 < nums1.size() && m.find({b,a+1}) == m.end()) {pq.push({nums1[a+1]+nums2[b],{b,a+1}});m[{b,a+1}] = 1;}
+            if (b+1 < nums2.size() && visited.find({a,b+1}) == visited.end()) {pq.push({nums1[a]+nums2[b+1],{a,b+1}});visited.insert({a,b+1});}
+            if (a+1 < nums1.size() && visited.find({a+1,b}) == visited.end()) {pq.push({nums1[a+1]+nums2[b],{a+1,b}});visited.insert({a+1,b});}
 
             pq.pop();
         }
