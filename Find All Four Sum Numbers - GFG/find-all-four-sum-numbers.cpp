e8c9f344daad2abc089926e2365762247a771b67
Vaibhav Arora <84830429+VaibhavArora314@users.noreply.github.com>
2023-09-26 10:08:13 +0530

    Added solution - LeetHub

--- Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
@@ -10,36 +10,35 @@ class Solution{
     public:
     // arr[] : int input array of integers
     // k : the quadruple sum required
-    vector<vector<int> > fourSum(vector<int> &arr, int target) {
+    vector<vector<int> > fourSum(vector<int> &arr, int k) {
         sort(arr.begin(),arr.end());
+        
         vector<vector<int>> ans;
         int n = arr.size();
-       
         for (int i=0;i<n;i++) {
             if (i>0 && arr[i] == arr[i-1]) continue;
+            
             for (int j=i+1;j<n;j++) {
-                if (j>i+1 && arr[j] == arr[j-1]) continue;
+                if (arr[j] == arr[j-1] && j>i+1) continue;
                 
-                int l=j+1,r=n-1,x=target - arr[i] - arr[j];
-                // cout<<i<<" "<<arr[i]<<" "<<j<<" "<<arr[j]<<endl;
+                int l=j+1,r=n-1,t=k-arr[i]-arr[j];
                 while (l<r) {
-                    // cout<<arr[l]<<" "<<arr[r]<<" "<<arr[l] + arr[r]<<" "<<x<<endl;
-                    if (arr[l] + arr[r] == x) {
+                    if (arr[l] + arr[r] == t) {
                         ans.push_back({arr[i],arr[j],arr[l],arr[r]});
+                        
                         l++;
-                        while (l<r && arr[l-1] == arr[l]) l++;
                         r--;
-                        while (l<r && arr[r+1] == arr[r]) r--;
-                    } else if (arr[l] + arr[r] > x) r--;
+                        while (l<r && arr[l] == arr[l-1]) l++;
+                        while (l<r && arr[r] == arr[r+1]) r--;
+                    } 
+                    else if (arr[l] + arr[r] > t) r--;
                     else l++;
                 }
             }
         }
-       
         return ans;
     }
 };
-
 
 //{ Driver Code Starts.
 int main() {
