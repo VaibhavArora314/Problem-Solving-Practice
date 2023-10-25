f040a2eb22d5b0224fee121975e91e5548c129a3
Vaibhav Arora <84830429+VaibhavArora314@users.noreply.github.com>
2023-10-25 11:38:12 +0530

    Time: 0 ms (100%), Space: 6.2 MB (65.8%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -1,14 +1,11 @@
 class Solution {
-    int helper(int curN,int k) {
-        if (curN == 0) return 0;
-
-        int prevK = k/2;
-        int prevV = helper(curN-1,prevK);
-        if (k == prevK*2) return prevV;
-        return prevV == 1 ? 0 : 1;
-    }
 public:
     int kthGrammar(int n, int k) {
-        return helper(n-1,k-1);
+        if (n == 1) return 0;
+
+        int prevK = (k+1)/2;
+        int prevV = kthGrammar(n-1,prevK);
+        if (k == prevK*2-1) return prevV;
+        return prevV == 1 ? 0 : 1;
     }
 };
